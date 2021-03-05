65c2a881869816d4fdc754ed21e42fa458abf8c2
bardurc <bardurc@users.noreply.github.com>
2021-03-05 21:45:16 +0000

    Create get_filenames.py

---
+++ get_filenames.py
@@ -0,0 +1,11 @@
+# Necessary import
+import os
+
+# os.listdir() returns all filenames in the current working directory
+# output is a list
+print(os.listdir())
+
+
+# Run some code over each file
+for fname in os.listdir():
+    [BLOCK OF CODE]
