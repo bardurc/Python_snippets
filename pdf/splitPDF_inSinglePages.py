578b7ac2d7de8597e0cfb178be0d044cb2bf90d7
bardurc <bardurc@users.noreply.github.com>
2021-06-08 08:11:32 +0100

    Create splitPDF_inSinglePages.py

---
+++ pdf/splitPDF_inSinglePages.py
@@ -0,0 +1,17 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+
+fname = 'ENTER INPUT FILENAME'
+
+# Open pdf file as binary (rb)
+inp = PdfFileReader(open(fname, 'rb'))
+
+# Iterate through all pages in the pdf
+for i in range(inp.numPages):
+    output = PdfFileWriter()
+    # Split pdf in single pages
+    output.addPage(inp.getPage(i))
+    # Write each page to new file
+    # Remember to open output file as binary (wb)
+    with open(f'pdf_page_{i}.pdf', 'wb') as outstream:
+        output.write(outstream)
